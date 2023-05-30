9df38bcbb27698fdec18f878b2dd84a3ddeac2c6
Jürgen Mikk <jurgen.mikk@khk.ee>
2023-05-30 18:47:41 +0300

    EP-implemented-link-element-hover-state-styling-in-navigation-component

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,22 +1,55 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Navigation.scss';
 import Logo from '../assets/logo.svg';
 import Avatar from '../assets/image-avatar.png';
 import {BsCart3} from 'react-icons/bs';
 
 function Navigation() {
+
+    const [hoveredLink, setHovered] = useState<string>();
+
+    const handleHoveredLink = (e: React.MouseEvent<HTMLAnchorElement>) => {
+        setHovered(e.currentTarget.innerHTML);
+    }
+
+    const handleHoveredLinkStyle = (targetElement: string) => {
+        return hoveredLink === targetElement ? 'hovered' : '';
+    }
+
+    const handleResetHoveredState = () => {
+        setHovered('');
+    }
+
+    const anchorTitles = [
+        "Learn more about our collections",
+        "Discover our men's section",
+        "Explore our women's section",
+        "Find out more about our company",
+        "Contact us for any inquiries"
+    ]
+
     return (
         <div className="main-nav-container">
             <img
                 src={Logo}
                 alt="logo"
             />
-            <div className="links-container">
-                <a href="/collections" title="Learn more about our collections">Collections</a>
-                <a href="/men" title="Discover our men's section">Men</a>
-                <a href="/women" title="Explore our women's section">Women</a>
-                <a href="/about" title="Find out more about our company">About</a>
-                <a href="/contact" title="Contact us for any inquiries">Contact</a>
+            <div className="links-container" onMouseLeave={handleResetHoveredState}>
+                <a href="/collections" id={handleHoveredLinkStyle('Collections')} title={anchorTitles[0]} onMouseEnter={e => handleHoveredLink(e)}>
+                    Collections
+                </a>
+                <a href="/men" id={handleHoveredLinkStyle('Men')} title={anchorTitles[1]} onMouseEnter={e => handleHoveredLink(e)}>
+                    Men
+                </a>
+                <a href="/women" id={handleHoveredLinkStyle('Women')} title={anchorTitles[2]} onMouseEnter={e => handleHoveredLink(e)}>
+                    Women
+                </a>
+                <a href="/about" id={handleHoveredLinkStyle('About')} title={anchorTitles[3]} onMouseEnter={e => handleHoveredLink(e)}>
+                    About
+                </a>
+                <a href="/contact" id={handleHoveredLinkStyle('Contact')} title={anchorTitles[4]} onMouseEnter={e => handleHoveredLink(e)}>
+                    Contact
+                </a>
             </div>
             <div className="interactive-container">
                 <BsCart3
