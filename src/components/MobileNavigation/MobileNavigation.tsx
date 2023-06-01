56422d06bf2fbd7584dbfb9b217fa460990f2e97
Jürgen Mikk <jurgen.mikk@khk.ee>
2023-06-01 16:51:35 +0300

    EP-implemented-mobile-navigation-sidebar-menu

--- src/components/MobileNavigation/MobileNavigation.tsx
+++ src/components/MobileNavigation/MobileNavigation.tsx
@@ -1,16 +1,63 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './MobileNavigation.scss';
 import Logo from '../assets/logo.svg';
 import Avatar from '../assets/image-avatar.png';
 import {BsCart3} from 'react-icons/bs';
 import {GiHamburgerMenu} from 'react-icons/gi';
+import {IoMdClose} from 'react-icons/io';
 
 function MobileNavigation() {
+
+    const [openSidebarMenu, setOpenSidebarMenu] = useState<boolean>(false);
+
+    const handleSidebarMenuPopup = () => {
+        setOpenSidebarMenu(true);
+    }
+
+    const handleSidebarMenuClose = () => {
+        setOpenSidebarMenu(false);
+    }
+
+    const anchorTitles = [
+        "Learn more about our collections",
+        "Discover our men's section",
+        "Explore our women's section",
+        "Find out more about our company",
+        "Contact us for any inquiries"
+    ]
+
     return (
         <div className="main-mobileNav-container">
             <div className="nav-container">
+                {openSidebarMenu &&
+                    <div className="sidebarMenu-container">
+                        <div className="sidebar-close">
+                            <IoMdClose
+                                onClick={handleSidebarMenuClose}
+                            />
+                        </div>
+                        <div className="sidebar-links">
+                            <a href="/collections" title={anchorTitles[0]}>
+                                Collections
+                            </a>
+                            <a href="/men" title={anchorTitles[1]}>
+                                Men
+                            </a>
+                            <a href="/women" title={anchorTitles[2]}>
+                                Women
+                            </a>
+                            <a href="/about" title={anchorTitles[3]}>
+                                About
+                            </a>
+                            <a href="/contact" title={anchorTitles[4]}>
+                                Contact
+                            </a>
+                        </div>
+                    </div>
+                }
                 <GiHamburgerMenu
                     id="links"
+                    onClick={handleSidebarMenuPopup}
                 />
                 <img
                     src={Logo}
