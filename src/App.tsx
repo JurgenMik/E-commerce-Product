b92e653e155d65737800fd28904680d3b0051947
Jürgen Mikk <jurgen.mikk@khk.ee>
2023-06-04 23:22:31 +0300

    EP-added-the-product-heading-section

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,28 @@ function App() {
             :
             <Navigation />
         }
+        <div className="product-content-container">
+            <div className="product-images">
+
+            </div>
+            <div className="product-details">
+                <div className="heading">
+                    <h1 id="enterprise">sneaker company</h1>
+                    <h1 id="title">Fall Limited Edition Sneakers</h1>
+                    <p>
+                        These low-profile sneakers are your perfect casual wear companion.
+                        Featuring a durable rubber outer sole, they'll withstand everything
+                        the weather can offer.
+                    </p>
+                </div>
+                <div className="price">
+
+                </div>
+                <div className="interaction">
+
+                </div>
+            </div>
+        </div>
     </div>
   );
 }
