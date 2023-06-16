85c142757832dc58c20742a708cd900cf40af505
Jürgen Mikk <jurgen.mikk@khk.ee>
2023-06-17 02:56:10 +0300

    EP-implemented-change-product-feature-based-on-user-selection

--- src/components/ProductFeature/ProductFeature.tsx
+++ src/components/ProductFeature/ProductFeature.tsx
@@ -1,31 +1,65 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './ProductFeature.scss';
 import ThumbnailOne from '../assets/product/image-product-1-thumbnail.jpg';
 import ThumbnailTwo from '../assets/product/image-product-2-thumbnail.jpg';
 import ThumbnailThree from '../assets/product/image-product-3-thumbnail.jpg';
 import ThumbnailFour from '../assets/product/image-product-4-thumbnail.jpg';
 import FeatureOne from '../assets/product/image-product-1.jpg';
+import FeatureTwo from '../assets/product/image-product-2.jpg';
+import FeatureThree from '../assets/product/image-product-3.jpg';
+import FeatureFour from '../assets/product/image-product-4.jpg';
 
 function ProductFeature() {
 
+    const [selectedThumbnail, setSelected] = useState<string>(FeatureOne);
+
+    const handleProductFeatureChange = (selected: string) => {
+        setSelected(selected);
+    }
+
     const productImages = [
-        ThumbnailOne,
-        ThumbnailTwo,
-        ThumbnailThree,
-        ThumbnailFour
-    ]
+        {
+            product_thumbnail: ThumbnailOne,
+            product_feature: FeatureOne
+        },
+        {
+            product_thumbnail: ThumbnailTwo,
+            product_feature: FeatureTwo
+        },
+        {
+            product_thumbnail: ThumbnailThree,
+            product_feature: FeatureThree
+        },
+        {
+            product_thumbnail: ThumbnailFour,
+            product_feature: FeatureFour
+        },
+    ];
 
     return (
         <div className="main-feature-container">
             <div className="product-feature">
                 <img
-                    src={FeatureOne}
+                    src={selectedThumbnail}
                     alt="product-feature"
                 />
             </div>
             <div className="product-images">
-                {productImages.map((image: string, index: number) => {
-                    return <img src={image} key={index} alt="product-thumbnail" />
+                {productImages.map((image: {product_thumbnail: string, product_feature: string}, index: number) => {
+                    return (
+                        <div className="thumbnail-container" key={index}>
+                            <img
+                                src={image.product_thumbnail}
+                                alt="product-thumbnail"
+                                onClick={() =>
+                                    handleProductFeatureChange(image.product_feature)
+                                }
+                            />
+                            {image.product_feature === selectedThumbnail &&
+                                <span id="overlay" />
+                            }
+                        </div>
+                    )
                 })}
             </div>
         </div>
